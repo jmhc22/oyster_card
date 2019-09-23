b43ee0502ea1eb369f4c9d8f4fa84f9e7b99e99a
Nabille Helal <nabilleh@outlook.com>
2019-09-23 14:35:32 +0100

    complete challenge #04: initialize card with zero balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,14 @@
-describe Oystercard do
+require 'oyster_card'
+
+describe OysterCard do
+
+  describe "New card is issued to a customer" do
+    it "The card should have a zero balance" do
+      expect(subject.balance).to eq 0
+    end
+
+
+
+  end
+
 end
