51dda3a852bc470ff84b9cbf9ad529542050b82b
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-24 14:51:53 +0100

    #Step 11: Adds entry station variable

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,31 +1,34 @@
 class OysterCard
 
   BALANCE_CAP = 90
-  MINIMUM_FARE = 1 
+  MINIMUM_FARE = 1
 
-  attr_reader :balance, :in_journey
+  attr_reader :balance, :in_journey, :entry_station
 
   def initialize
     @balance = 0
     @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(amount)
     @balance >=BALANCE_CAP ? raise("Maximum balance of #{BALANCE_CAP} reached") : @balance += amount
   end
 
-  def touch_in
+  def touch_in(entry_station)
     raise "Fare exceeds available balance" if @balance < MINIMUM_FARE
+    @entry_station = entry_station
     @in_journey = true
   end
 
   def touch_out
     deduct
+    @entry_station = nil
     @in_journey = false
   end
 
-  private 
-  
+  private
+
   def deduct
     @balance -= MINIMUM_FARE
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,16 +2,25 @@ require 'oyster_card'
 
 describe OysterCard do
 
+  let(:station_a) { double :station_a }
+
+  before(:each) do
+    subject.top_up(20)
+  end
+
   describe "New card is issued to a customer" do
     it "The card should have a zero balance" do
+      subject = OysterCard.new
       expect(subject.balance).to eq 0
     end
   end
 
   describe '#top up' do
     it 'adds money to the card and returns the card balance' do
-      subject.top_up(10)
-      expect(subject.balance).to eq 10
+      initial_amount = subject.balance
+      topup_amount = 10
+      subject.top_up(topup_amount)
+      expect(subject.balance).to eq initial_amount + topup_amount
     end
 
     it 'caps the balance at BALANCE_CAP' do
@@ -23,12 +32,18 @@ describe OysterCard do
   describe "#touch_in" do
     it "should set the in journey status to true" do
       subject.top_up(90)
-      subject.touch_in
+      subject.touch_in(station_a)
       expect(subject.in_journey).to eq true
     end
 
     it "raises an error if card does not have required minimum balance" do
-      expect{subject.touch_in}.to raise_error("Fare exceeds available balance")
+      subject = OysterCard.new
+      expect{subject.touch_in(station_a)}.to raise_error("Fare exceeds available balance")
+    end
+
+    it "stores tap in station as a variable" do
+      subject.touch_in(station_a)
+      expect(subject.entry_station).to eq station_a
     end
   end
 
@@ -40,10 +55,15 @@ describe OysterCard do
 
     it 'reduces card balance by minimum fare' do
       subject.top_up(10)
-      subject.touch_in
+      subject.touch_in(station_a)
       expect{subject.touch_out}.to change{subject.balance}.by(-OysterCard::MINIMUM_FARE)
     end
 
+    it 'tapping out will reset entry station to nil' do
+      subject.touch_in(station_a)
+      subject.touch_out
+      expect(subject.entry_station).to eq nil
+    end
   end
 
 
